fec42bf410f667c927cdeefa6fd9c2d9ce3d0d9a
Bert Laverman <bert.laverman@rakis.nl>
2023-08-17 15:47:52 +0200

    Formatting mistake

--- src/i2c-test.cpp
+++ src/i2c-test.cpp
@@ -19,7 +19,7 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] const char** argv )
     RaspberryPi& berry(*RaspberryPi::instance());
 
     for (unsigned i = 0x20 ; i < 0x30 ; i++) {
-        std::cerr << "Device " << hex(i) << ": ";
+        std::cerr << "Device 0x" << hex(i >> 4) << hex(i & 0x0f) << ": ";
         if (berry.i2c().write(i, std::span<uint8_t>())) {
             std::cerr << "Found" << std::endl;
         } else {
